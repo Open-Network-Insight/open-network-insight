1c892d6b0535456020887394e0f1160dba04aac3
natedogs911 <natedogs911@gmail.com>
2016-11-22 16:41:29 -0800

    update proxy pipeline with ConfigParser

--- spot-ingest/pipelines/proxy/collector.py
+++ spot-ingest/pipelines/proxy/collector.py
@@ -1,7 +1,7 @@
 #!/bin/env python
 
 import logging
-import json
+import ConfigParser
 import os
 import sys
 import copy
@@ -11,14 +11,15 @@ from multiprocessing import Pool
 from common.file_collector import FileWatcher
 import time
 
+
 class Collector(object):
-    
+
     def __init__(self,hdfs_app_path,kafka_topic,conf_type):
-        
+
         self._initialize_members(hdfs_app_path,kafka_topic,conf_type)
-        
+
     def _initialize_members(self,hdfs_app_path,kafka_topic,conf_type):
-        
+
         # getting parameters.
         self._logger = logging.getLogger('SPOT.INGEST.PROXY')
         self._hdfs_app_path = hdfs_app_path
@@ -28,75 +29,75 @@ class Collector(object):
         self._script_path = os.path.dirname(os.path.abspath(__file__))
 
         # read proxy configuration.
-        conf_file = "{0}/ingest_conf.json".format(os.path.dirname(os.path.dirname(self._script_path)))
-        conf = json.loads(open(conf_file).read())
-        self._message_size = conf["kafka"]["message_size"]
-        self._conf = conf["pipelines"][conf_type]
+        conf_file = "/etc/spot.conf"
+        self._conf = ConfigParser.SafeConfigParser()
+        self._conf.read(conf_file)
+        self._message_size = self._conf.get('ingest', "message_size")
 
         # get collector path.
-        self._collector_path = self._conf['collector_path']
+        self._collector_path = self._conf.get(conf_type, 'collector_path')
 
         #get supported files
-        self._supported_files = self._conf['supported_files']
+        self._supported_files = self._conf.get(conf_type, 'supported_files')
 
         # create collector watcher
         self._watcher = FileWatcher(self._collector_path,self._supported_files)
 
-        # Multiprocessing. 
-        self._processes = conf["collector_processes"]
-        self._ingestion_interval = conf["ingestion_interval"]
+        # Multiprocessing.
+        self._processes = self._conf.get('ingest', "collector_processes")
+        self._ingestion_interval = self._conf.get('ingest', "ingestion_interval")
         self._pool = Pool(processes=self._processes)
 
     def start(self):
 
         self._logger.info("Starting PROXY collector")
-        self._watcher.start()   
-    
+        self._watcher.start()
+
         try:
             while True:
                 #self._ingest_files()
-                self._ingest_files_pool()              
+                self._ingest_files_pool()
                 time.sleep(self._ingestion_interval)
         except KeyboardInterrupt:
-            self._logger.info("Stopping Proxy collector...")  
-            Util.remove_kafka_topic(self._kafka_topic.Zookeeper,self._kafka_topic.Topic,self._logger)          
+            self._logger.info("Stopping Proxy collector...")
+            Util.remove_kafka_topic(self._kafka_topic.Zookeeper,self._kafka_topic.Topic,self._logger)
             self._watcher.stop()
             self._pool.terminate()
-            self._pool.close()            
+            self._pool.close()
             self._pool.join()
-             
+
 
     def _ingest_files_pool(self):
-            
-       
+
+
         if self._watcher.HasFiles:
-            
+
             for x in range(0,self._processes):
                 file = self._watcher.GetNextFile()
                 resutl = self._pool.apply_async(ingest_file,args=(file,self._message_size,self._kafka_topic.Topic,self._kafka_topic.BootstrapServers))
-                #resutl.get() # to debug add try and catch.
-                if  not self._watcher.HasFiles: break    
+                # resutl.get() # to debug add try and catch.
+                if  not self._watcher.HasFiles: break
         return True
 
 
 def ingest_file(file,message_size,topic,kafka_servers):
-    
+
     logger = logging.getLogger('SPOT.INGEST.PROXY.{0}'.format(os.getpid()))
-    try:        
+    try:
         message = ""
-        logger.info("Ingesting file: {0} process:{1}".format(file,os.getpid())) 
+        logger.info("Ingesting file: {0} process:{1}".format(file,os.getpid()))
         with open(file,"rb") as f:
             for line in f:
                 message += line
                 if len(message) > message_size:
                     KafkaTopic.SendMessage(message,kafka_servers,topic,0)
                     message = ""
-            #send the last package.        
-            KafkaTopic.SendMessage(message,kafka_servers,topic,0)            
+            #send the last package.
+            KafkaTopic.SendMessage(message,kafka_servers,topic,0)
         rm_file = "rm {0}".format(file)
         Util.execute_cmd(rm_file,logger)
         logger.info("File {0} has been successfully sent to Kafka Topic: {1}".format(file,topic))
 
-    except Exception as err:        
+    except Exception as err:
         logger.error("There was a problem, please check the following error message:{0}".format(err.message))
         logger.error("Exception: {0}".format(err))
--- spot-ingest/pipelines/proxy/worker.py
+++ spot-ingest/pipelines/proxy/worker.py
@@ -1,7 +1,7 @@
 #!/bin/env python
 import os
 import logging
-import json
+import ConfigParser
 
 from common.utils import Util
 
@@ -13,7 +13,7 @@ class Worker(object):
         self._initialize_members(db_name,hdfs_app_path,kafka_consumer,conf_type,processes)
 
     def _initialize_members(self,db_name,hdfs_app_path,kafka_consumer,conf_type,processes):
-        
+
         # get logger instance.
         self._logger = Util.get_logger('SPOT.INGEST.WRK.PROXY')
 
@@ -23,43 +23,40 @@ class Worker(object):
 
         # read proxy configuration.
         self._script_path = os.path.dirname(os.path.abspath(__file__))
-        conf_file = "{0}/ingest_conf.json".format(os.path.dirname(os.path.dirname(self._script_path)))
-        conf = json.loads(open(conf_file).read())
-        self._spark_conf  = conf["spark-streaming"]
-        self._conf = conf["pipelines"][conf_type]
+        conf_file = "/etc/spot.conf"
+        self._conf = ConfigParser.SafeConfigParser()
+        self._conf.read(conf_file)
         self._processes = processes
 
     def start(self):
 
-        self._logger.info("Creating Spark Job for topic: {0}".format(self._kafka_consumer.Topic))                
+        self._logger.info("Creating Spark Job for topic: {0}".format(self._kafka_consumer.Topic))
 
         # parser
-        parser = self._conf["parser"]
+        parser = self._conf.get('proxy', "parser")
 
-        #spark conf
-        diver_memory = self._spark_conf["driver_memory"]
-        num_exec = self._spark_conf["spark_exec"]
-        exec_memory = self._spark_conf["spark_executor_memory"]
-        exec_cores = self._spark_conf["spark_executor_cores"]
-        batch_size = self._spark_conf["spark_batch_size"]
-        
+        # spark conf
+        diver_memory = self._conf.get('ingest', "driver_memory")
+        num_exec = self._conf.get('ingest', "spark_exec")
+        exec_memory = self._conf.get('ingest', "spark_executor_memory")
+        exec_cores = self._conf.get('ingest', "spark_executor_cores")
+        batch_size = self._conf.get('ingest', "spark_batch_size")
+
         jar_path = os.path.dirname(os.path.dirname(self._script_path))
-        # spark job command.          
+        # spark job command.
         spark_job_cmd = ("spark-submit --master yarn "
-                        "--driver-memory {0} "
-                        "--num-executors {1} "
-                        "--conf spark.executor.memory={2} "
-                        "--conf spark.executor.cores={3} "
-                        "--jars {4}/common/spark-streaming-kafka-0-8-assembly_2.11-2.0.0.jar "
-                        "{5}/{6} "
-                        "-zk {7} "
-                        "-t {8} "
-                        "-db {9} "
-                        "-dt {10} "
-                        "-w {11} "
-                        "-bs {12}".format(diver_memory,num_exec,exec_memory,exec_cores,jar_path,self._script_path,parser,self._kafka_consumer.ZookeperServer,self._kafka_consumer.Topic,self._db_name,"proxy",self._processes,batch_size))
-        
+                         "--driver-memory {0} "
+                         "--num-executors {1} "
+                         "--conf spark.executor.memory={2} "
+                         "--conf spark.executor.cores={3} "
+                         "--jars {4}/common/spark-streaming-kafka-0-8-assembly_2.11-2.0.0.jar "
+                         "{5}/{6} "
+                         "-zk {7} "
+                         "-t {8} "
+                         "-db {9} "
+                         "-dt {10} "
+                         "-w {11} "
+                         "-bs {12}".format(diver_memory,num_exec,exec_memory,exec_cores,jar_path,self._script_path,parser,self._kafka_consumer.ZookeperServer,self._kafka_consumer.Topic,self._db_name,"proxy",self._processes,batch_size))
+
         # start spark job.
         Util.execute_cmd(spark_job_cmd,self._logger)
-
-        
